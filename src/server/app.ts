9f5fd35176a94f167ed6ccdffdeb58b78c693fba
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-02 21:48:54 +0200

    Add handleEndPointNotFound middlware

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import monumentsRouter from "../router/monumentsRouter.js";
+import morgan from "morgan";
+import monumentsRouter from "../monument/router/monumentsRouter.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound/handleEntpointNotFount.js";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use("/monuments", monumentsRouter);
+app.use(handleEndpointNotFound);
 
 export default app;
