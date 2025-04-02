7cf5d02be269864ad80082a856115277a5c1cda6
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-02 13:59:35 +0200

    Fix incorrect getAllMonuments method implementation

--- src/monument/controller/MonumentController.ts
+++ src/monument/controller/MonumentController.ts
@@ -1,11 +1,13 @@
+import { Request, Response } from "express";
 import { MonumentStructure } from "../types.js";
+import MonumentControllerStructure from "./types.js";
 
-class MonumentController {
+class MonumentController implements MonumentControllerStructure {
   constructor(private monuments: MonumentStructure[]) {}
 
-  getAllMonuments(): MonumentStructure[] {
-    return this.monuments;
-  }
+  public getAllMonuments = (_req: Request, res: Response): void => {
+    res.status(200).json({ monuments: this.monuments });
+  };
 }
 
 export default MonumentController;
