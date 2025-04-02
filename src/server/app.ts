4193bae1f62af7f9dcff1d47aa7cf04b05fb23f1
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-02 15:03:34 +0200

    Remove test code

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,5 @@
 import express from "express";
-import MonumentController from "../monument/controller/MonumentController.js";
-import { monuments } from "../monument/data/monuments.js";
 
 const app = express();
-
-const foo = new MonumentController(monuments);
-
-app.get("/", foo.getAllMonuments);
 
 export default app;
